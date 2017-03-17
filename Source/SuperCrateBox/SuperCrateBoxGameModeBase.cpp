1fbd42d2ae69f2fca36776f9cab44aee49c02a85
Razvan Antohi <Razvan Antohi>
2017-03-17 22:27:47 +0200

    Initial commit

---
+++ Source/SuperCrateBox/SuperCrateBoxGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SuperCrateBox.h"
+#include "SuperCrateBoxGameModeBase.h"
+
+ASuperCrateBoxGameModeBase::ASuperCrateBoxGameModeBase()
+{
+
+}
+
+
---
+++ Source/SuperCrateBox/SuperCrateBoxGameModeBase.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SuperCrateBoxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERCRATEBOX_API ASuperCrateBoxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+
+    ASuperCrateBoxGameModeBase();
+  	
+	
+};
